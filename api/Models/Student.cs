c085d1b8b035cb65c10aa5798027d1c1b667f813
Mykyta Tretiakt <nixer@ukr.net>
2023-05-20 01:28:59 +0300

    change docker

--- api/Models/Student.cs
+++ api/Models/Student.cs
@@ -5,8 +5,8 @@ namespace api.Models
   public class Student
   {
     public int ID { get; set; }
-    public string LastName { get; set; }
-    public string FirstMidName { get; set; }
+    public string? LastName { get; set; }
+    public string? FirstMidName { get; set; }
     public DateTime EnrollmentDate { get; set; }
   }
 }
